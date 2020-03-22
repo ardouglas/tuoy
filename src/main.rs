b7f3283d4d7d4c8669e575a2a969be12d2d0073e
Andrew Douglas <andrewrobertdouglas@gmail.com>
2020-03-22 09:27:10 -0500

    Switched main table view to active stations list.

--- src/main.rs
+++ src/main.rs
@@ -15,32 +15,34 @@ use std::{
     time::Duration,
 };
 use crossterm::event::MouseEvent;
+use roxmltree::Node;
 
 mod net;
+mod state;
 
 pub enum Event<I,J> {
     Key(I),
     Mouse(J),
 }
 
-pub struct StatefulTable<'a> {
+pub struct StatefulTable {
     state: TableState,
-    items: Vec<Vec<&'a str>>,
+    items: Vec<Vec<String>>,
 }
 
-impl<'a> StatefulTable<'a> {
-    fn new(rows: Vec<&'a str>) -> StatefulTable<'a> {
-        let mut items = Vec::new();
-        for row in rows {
-            if !row.starts_with('#') {
-                let split = row.split_ascii_whitespace().collect::<Vec<&str>>();
-                items.push(split);
-            }
-        }
+impl StatefulTable {
+    fn new(rows: Vec<Vec<String>>) -> StatefulTable {
+        //let mut items = Vec::new();
+        //for row in rows {
+        //    if !row.starts_with('#') {
+        //        let split = row.split_ascii_whitespace().collect::<Vec<&str>>();
+        //        items.push(split);
+        //    }
+        //}
 
         StatefulTable {
             state: TableState::default(),
-            items,
+            items: rows,
         }
     }
     pub fn next(&mut self) {
@@ -74,11 +76,15 @@ impl<'a> StatefulTable<'a> {
 
 #[tokio::main]
 async fn main() -> Result<(), failure::Error> {
-    let latest_obs_resp = net::get_latest_obs().await.unwrap();
+    let active_stations_resp = net::get_active_stations().await.unwrap();
 
-    let body = latest_obs_resp.text().await.unwrap();
-    let split = body.split('\n');
-    let row_strs = split.collect::<Vec<&str>>();
+    let body = active_stations_resp.text().await.unwrap();
+    let active_stations = xml_to_stations(body);
+    //let mut rows = Vec::new();
+    let rows: Vec<Vec<String>> = active_stations.iter().map(|a| a.to_row()).collect();
+
+    //let split = body.split('\n');
+    //let row_strs = split.collect::<Vec<&str>>();
 
     enable_raw_mode()?;
 
@@ -105,7 +111,7 @@ async fn main() -> Result<(), failure::Error> {
         }
     });
 
-    let mut table = StatefulTable::new(row_strs);
+    let mut table = StatefulTable::new(rows);
 
     // Input
     loop {
@@ -118,10 +124,11 @@ async fn main() -> Result<(), failure::Error> {
             let selected_style = Style::default().fg(Color::LightCyan);
             let normal_style = Style::default().fg(Color::White);
 
-            let header = [
-                "stn", "lat", "lon", "year", "mo", "day", "hr", "min", "wdir", "wspd", "gst",
-                "wvht", "dpd", "apd", "mwd", "pres", "ptdy", "atmp", "wtmp", "dewp", "vis", "tide",
-            ];
+//            let header = [
+//                "stn", "lat", "lon", "year", "mo", "day", "hr", "min", "wdir", "wspd", "gst",
+//                "wvht", "dpd", "apd", "mwd", "pres", "ptdy", "atmp", "wtmp", "dewp", "vis", "tide",
+//            ];
+            let header = ["station", "name", "lat", "lon", "program", "kind", "met", "currents", "water quality", "dart"];
             let rows = table
                 .items
                 .iter()
@@ -130,32 +137,20 @@ async fn main() -> Result<(), failure::Error> {
                 .block(
                     Block::default()
                         .borders(Borders::ALL)
-                        .title("Latest Observations"),
+                        .title("Active Stations"),
                 )
                 .highlight_style(selected_style)
                 .widths(&[
-                    Constraint::Percentage(7),
                     Constraint::Percentage(5),
-                    Constraint::Percentage(5),
-                    Constraint::Percentage(5),
+                    Constraint::Percentage(25),
+                    Constraint::Percentage(4),
+                    Constraint::Percentage(4),
+                    Constraint::Percentage(15),
+                    Constraint::Percentage(10),
                     Constraint::Percentage(3),
                     Constraint::Percentage(3),
+                    Constraint::Percentage(10),
                     Constraint::Percentage(3),
-                    Constraint::Percentage(3),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
-                    Constraint::Percentage(4),
                 ]);
             f.render_stateful_widget(t, rects[0], &mut table.state);
         })?;
@@ -189,4 +184,59 @@ async fn main() -> Result<(), failure::Error> {
     }
 
     Ok(())
+}
+
+fn xml_to_stations(body: String) -> Vec<ActiveStation>{
+    let doc = roxmltree::Document::parse(&body).unwrap();
+    let active_stations: Vec<ActiveStation> = doc.descendants()
+        .filter(|n| n.tag_name().name() == "station")
+        .map(|n| ActiveStation::from_node(n))
+        .collect();
+    active_stations
+}
+
+struct ActiveStation{
+    id: String,
+    name: String,
+    lat: String,
+    lon: String,
+    program: String,
+    kind: String,
+    met: String,
+    currents: String,
+    water_quality: String,
+    dart: String,
+}
+
+impl ActiveStation{
+    fn from_node(node: Node)-> ActiveStation{
+        ActiveStation{
+            id: node.attribute("id").map_or(String::from("whew, no id? how'd that happen"), |a| a.to_owned()),
+            name: node.attribute("name").map_or(String::from("whew, no name? how'd that happen"), |a| a.to_owned()),
+            lat: node.attribute("lat").map(|a| a.to_owned()).unwrap(),
+            lon: node.attribute("lon").map(|a| a.to_owned()).unwrap(),
+            program: node.attribute("pgm").map_or(String::from("whew, no pgm? how'd that happen"), |a| a.to_owned()),
+            kind: node.attribute("type").map_or(String::from("whew, no kind? how'd that happen"), |a| a.to_owned()),
+            met: node.attribute("met").map_or(String::from("n"),|a| a.to_owned()),
+            currents: node.attribute("currents").map_or(String::from("n"),|a| a.to_owned()),
+            water_quality: node.attribute("waterquality").map_or(String::from("n"),|a| a.to_owned()),
+            dart: node.attribute("dart").map_or(String::from("n"),|a| a.to_owned()),
+        }
+    }
+
+    fn to_row(&self)->Vec<String>{
+        let mut row = Vec::new();
+        row.push(self.id.clone());
+        row.push(self.name.clone());
+        row.push(self.lat.to_string());
+        row.push(self.lon.to_string());
+        row.push(self.program.clone());
+        row.push(self.kind.clone());
+        row.push(self.met.clone());
+        row.push(self.currents.clone());
+        row.push(self.water_quality.clone());
+        row.push(self.dart.clone());
+
+        row
+    }
 }
--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,7 +1,11 @@
-//use reqwest::Client;
 use reqwest::Error;
 use reqwest::Response;
 
 pub async fn get_latest_obs() -> Result<Response, Error> {
     Ok(reqwest::get("https://www.ndbc.noaa.gov/data/latest_obs/latest_obs.txt").await?)
+}
+
+
+pub async fn get_active_stations() -> Result<Response, Error> {
+    Ok(reqwest::get("https://www.ndbc.noaa.gov/activestations.xml").await?)
 }
